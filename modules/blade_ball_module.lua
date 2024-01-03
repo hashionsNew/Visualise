4f69497871f82b48e2159eb6932efd619da5220c
hashions <101004266+hashionsNew@users.noreply.github.com>
2024-01-04 01:44:42 +0300

    Update blade_ball_module.lua

--- modules/blade_ball_module.lua
+++ modules/blade_ball_module.lua
@@ -3,6 +3,9 @@ local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
 local blade_ball_module = {}
 
+local parry_attempt = ReplicatedStorage.Remotes.ParryAttempt
+local parry_button_press = ReplicatedStorage.Remotes.ParryButtonPress
+
 
 function blade_ball_module.get_ball()
     for _, v in workspace.Balls:GetChidlren() do
@@ -13,8 +16,12 @@ function blade_ball_module.get_ball()
 end
 
 
-function blade_ball_module.parry(direction: Raycast)
-
+function blade_ball_module.parry(direction: Raycast, external: boolean)
+    if external then
+        
+    else
+        parry_button_press:Fire()
+    end
 end
 
 
