6e4594691aa5c93e3d29561c2f8fc07ad4bb076c
hashions <101004266+hashionsNew@users.noreply.github.com>
2024-01-04 01:47:40 +0300

    Update blade_ball_module.lua

--- modules/blade_ball_module.lua
+++ modules/blade_ball_module.lua
@@ -5,6 +5,7 @@ local blade_ball_module = {}
 
 local parry_attempt = ReplicatedStorage.Remotes.ParryAttempt
 local parry_button_press = ReplicatedStorage.Remotes.ParryButtonPress
+local ability_button_press = ReplicatedStorage.Remotes.AbilityButtonPress
 
 
 function blade_ball_module.get_ball()
@@ -18,15 +19,15 @@ end
 
 function blade_ball_module.parry(direction: Raycast, external: boolean)
     if external then
-        
+
     else
         parry_button_press:Fire()
     end
 end
 
 
-function blade_ball_module.ability(direction: Raycast)
-
+function blade_ball_module.ability()
+    ability_button_press:Fire()
 end
 
 
