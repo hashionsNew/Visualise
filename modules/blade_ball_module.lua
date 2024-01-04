e1a50583a7dc311808cad6fc01d4443e794faae6
hashions <101004266+hashionsNew@users.noreply.github.com>
2024-01-04 03:06:52 +0300

    Update blade_ball_module.lua

--- modules/blade_ball_module.lua
+++ modules/blade_ball_module.lua
@@ -3,13 +3,15 @@ local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
 local blade_ball_module = {}
 
+local net = ReplicatedStorage.Packages._Index.['sleitnick_net@0.1.0'].net
+
 local parry_attempt = ReplicatedStorage.Remotes.ParryAttempt
 local parry_button_press = ReplicatedStorage.Remotes.ParryButtonPress
 local ability_button_press = ReplicatedStorage.Remotes.AbilityButtonPress
 
 
 function blade_ball_module.get_ball()
-    for _, v in workspace.Balls:GetChidlren() do
+    for _, v in workspace.Balls:GetChildren() do
         if v:GetAttribute('realBall') then
             return v
         end
@@ -28,6 +30,13 @@ end
 
 function blade_ball_module.ability()
     ability_button_press:Fire()
+end
+
+
+function blade_ball_module.claim_playtime_rewards()
+    for _ = 1, 6 do
+        net['RF/ClaimPlaytimeReward']:InvokeServer(_)        
+    end
 end
 
 
