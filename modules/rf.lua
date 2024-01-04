d919df86b333841936edcbbcac2ba0051821d361
hashions <101004266+hashionsNew@users.noreply.github.com>
2024-01-04 20:28:38 +0300

    Update rf.lua

--- modules/rf.lua
+++ modules/rf.lua
@@ -1,5 +1,6 @@
 local LocalPlayer = game:GetService('Players').LocalPlayer
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
+local TweenService = game:GetService('TweenService')
 local RunService = game:GetService('RunService')
 local Lighting = game:GetService('Lighting')
 
@@ -20,7 +21,7 @@ combat_tab.create_label({name = 'AutoParry'})
 local auto_parry_toggle = combat_tab.create_toggle({name = 'Enabled', flag = 'auto_parry', checkbox = false, section = 'left', callback = function(state: boolean)
 end})
 
-local auto_parry_accuracy_slider = combat_tab.create_slider({name = 'Accuracy', flag = 'auto_parry_accuracy', maximum = 100, minimum = 1, value = 100, section = 'left', callback = function(state: boolean)
+local auto_parry_accuracy_slider = combat_tab.create_slider({name = 'Accuracy', flag = 'auto_parry_accuracy', maximum = 100, minimum = 1, value = 100, section = 'left', callback = function(state: number)
 end})
 
 local world_tab = main.create_tab({name = 'World'})
@@ -28,19 +29,15 @@ local world_tab = main.create_tab({name = 'World'})
 world_tab.create_label({name = 'WorldTime'})
 
 local world_time_toggle = world_tab.create_toggle({name = 'Enabled', flag = 'world_time', checkbox = false, section = 'left', callback = function(state: boolean)
-    local tween_time = 0.5 + (0.5 / (Lighting.ClockTime - state))
-
     if state then
-        TweenService:Create(Lighting, TweenInfo.new(tween_time), {ClockTime = ui_library.flags['world_time_value']})
+        TweenService:Create(Lighting, TweenInfo.new(0.5), {ClockTime = ui_library.flags['world_time_value']}):Play()
     else
-        TweenService:Create(Lighting, TweenInfo.new(tween_time), {ClockTime = 7})
+        TweenService:Create(Lighting, TweenInfo.new(0.5), {ClockTime = 7}):Play()
     end
 end})
 
-local world_time_value_slider = world_tab.create_slider({name = 'Time', flag = 'world_time_value', maximum = 12, minimum = 0, value = 0, section = 'left', callback = function(state: boolean)
-    local tween_time = 0.5 + (0.5 / (Lighting.ClockTime - state))
-    
-    TweenService:Create(Lighting, TweenInfo.new(tween_time), {ClockTime = state})
+local world_time_value_slider = world_tab.create_slider({name = 'Time', flag = 'world_time_value', maximum = 12, minimum = 0, value = 0, section = 'left', callback = function(state: number)
+    TweenService:Create(Lighting, TweenInfo.new(0.5), {ClockTime = state}):Play()
 end})
 
 
