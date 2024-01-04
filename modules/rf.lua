fe316628d3e6548e2d6cbf92898a8c309ea38d32
hashions <101004266+hashionsNew@users.noreply.github.com>
2024-01-04 18:58:59 +0300

    Update rf.lua

--- modules/rf.lua
+++ modules/rf.lua
@@ -3,15 +3,37 @@ local ReplicatedStorage = game:GetService('ReplicatedStorage')
 local RunService = game:GetService('RunService')
 
 local modules = {
-    --ui_library = loadstring(game:HttpGet(''))(),
     main = loadstring(game:HttpGet('https://raw.githubusercontent.com/hashionsNew/Visualise/main/modules/main.lua'))(),
     blade_ball = loadstring(game:HttpGet('https://raw.githubusercontent.com/hashionsNew/Visualise/main/modules/blade_ball_module.lua'))()
 }
 
 local parried = false
 
+local ui_library = loadstring(game:HttpGet('https://raw.githubusercontent.com/hashionsNew/Visualise/main/ui_library.lua'))()
+local main = ui_library.create()
+
+local combat_tab = main.create_tab({name = 'Combat'})
+
+combat_tab.create_label({name = 'AutoParry'})
+
+local auto_parry_toggle = combat_tab.create_toggle({name = 'Enabled', checkbox = false, flag = 'auto_parry', section = 'left', callback = function(state: boolean)
+end})
+
+local slider = combat_tab.create_slider({name = 'Accuracy', section = 'left', maximum = 100, minimum = 1, value = 80, callback = function(state: boolean)
+end})
+
 
 RunService.Heartbeat:Connect(function()
+    if not ui_library.flags['auto_parry'] then
+        return
+    end
+
+    if not modules.main.alive(LocalPlayer) then
+        print('player is not alive')
+
+        return
+    end
+
     local ball = modules.blade_ball.get_ball()
 
     if not ball then
